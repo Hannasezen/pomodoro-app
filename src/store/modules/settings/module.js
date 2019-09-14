102ebadcdabae9ed3a549f2dd5b024606a8ea582
Hanna Sezen <andreeva0649@gmail.com>
2019-09-14 20:43:10 +0300

    init settings store

--- src/store/modules/settings/module.js
+++ src/store/modules/settings/module.js
@@ -1,1 +1,12 @@
-export default {};
+import state from './state';
+import actions from './actions';
+import getters from './getters';
+import mutations from './mutations';
+
+export default {
+  namespaced: true,
+  actions,
+  getters,
+  mutations,
+  state,
+}
